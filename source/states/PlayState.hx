2190be1213d3dd1840a39679f7b4ba436110354a
WayloFunk <130512773+WayloFunk@users.noreply.github.com>
2024-11-12 22:51:21 -0500

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -74,16 +74,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Dave and bambi spam track', 0.2], //From 0% to 19%
+		['...', 0.4], //From 20% to 39%
+		['Noob', 0.5], //From 40% to 49%
+		['Get out', 0.6], //From 50% to 59%
+		['Nothing like Waylon', 0.69], //From 60% to 68%
+		['Haha', 0.7], //69%
+		['Eh close enough', 0.8], //From 70% to 79%
+		['Kinda like Waylon', 0.9], //From 80% to 89%
+		['Almost like Waylon!', 1], //From 90% to 99%
+		['Just like Waylon!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
 	//event variables
