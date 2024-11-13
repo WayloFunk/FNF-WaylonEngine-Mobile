cece439751ba028c8dd68020e71955b2063a4685
WayloFunk <130512773+WayloFunk@users.noreply.github.com>
2024-11-13 13:00:54 -0500

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -86,7 +86,7 @@ class TitleState extends MusicBeatState
 		#if CHECK_FOR_UPDATES
 		if(ClientPrefs.data.checkForUpdates && !closedState) {
 			trace('checking for update');
-			var http = new haxe.Http("https://raw.githubusercontent.com/MobilePorting/FNF-PsychEngine-Mobile/main/gitVersion.txt");
+			var http = new haxe.Http("https://raw.githubusercontent.com/WayloFunk/FNF-WaylonEngine-Mobile/main/gitVersion.txt");
 
 			http.onData = function (data:String)
 			{
