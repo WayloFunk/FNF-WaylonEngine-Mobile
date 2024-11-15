0ad4aa8031e1678af71d2ef796cfeba26f572f85
WayloFunk <130512773+WayloFunk@users.noreply.github.com>
2024-11-14 19:54:14 -0500

    Update GraphicsSettingsSubState.hx

--- source/options/GraphicsSettingsSubState.hx
+++ source/options/GraphicsSettingsSubState.hx
@@ -11,7 +11,7 @@ class GraphicsSettingsSubState extends BaseOptionsMenu
 		title = Language.getPhrase('graphics_menu', 'Graphics Settings');
 		rpcTitle = 'Graphics Settings Menu'; //for Discord Rich Presence
 
-		boyfriend = new Character(840, 170, 'bf', true);
+		boyfriend = new Character(840, 170, 'WaylonREM', true);
 		boyfriend.setGraphicSize(Std.int(boyfriend.width * 0.75));
 		boyfriend.updateHitbox();
 		boyfriend.dance();
